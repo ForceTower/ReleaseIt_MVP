805c88550d49af64daab7c4570043bd919490337
João Paulo Sena <joaopaulo761@gmail.com>
2020-02-04 16:47:20 -0300

    :tada: Batman! This commit has no parents

---
+++ app/src/main/java/dev/forcetower/cubicrectangle/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package dev.forcetower.cubicrectangle.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.forcetower.cubicrectangle.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
